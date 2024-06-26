93443969f50cdac16b17c451fea57a23bd44f55c
ludachrismonte <ludachrismonte@gmail.com>
2024-06-26 12:03:04 -0700

    aded header

--- c-arches-frontend/src/app/shared/shared.module.ts
+++ c-arches-frontend/src/app/shared/shared.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CharacterComponent } from './components/character/character.component';
+import { HeaderComponent } from "./components/header/header.component";
 import { BackgroundComponent } from './components/background/background.component';
 import { DialogueComponent } from './components/dialogue/dialogue.component';
 import { SceneComponent } from './components/scene/scene.component';
 
 @NgModule({
-  declarations: [CharacterComponent, BackgroundComponent, DialogueComponent, SceneComponent],
+  declarations: [CharacterComponent, HeaderComponent, BackgroundComponent, DialogueComponent, SceneComponent],
   imports: [CommonModule],
-  exports: [SceneComponent]
+  exports: [SceneComponent, HeaderComponent]
 })
 export class SharedModule { }
--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-P6J7NRFM.mjs";import"./chunk-VKYVB4JM.mjs";import"./chunk-7IXDZII2.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
+import{a}from"./chunk-KBGTBYTW.mjs";import"./chunk-IIN2YATN.mjs";import"./chunk-MGALPFGE.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
