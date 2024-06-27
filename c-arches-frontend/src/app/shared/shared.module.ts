8935572907031f19534af12d4bf4490817f208ea
ludachrismonte <ludachrismonte@gmail.com>
2024-06-26 23:22:14 -0400

    resources

--- c-arches-frontend/src/app/shared/shared.module.ts
+++ c-arches-frontend/src/app/shared/shared.module.ts
@@ -3,13 +3,27 @@ import { CommonModule } from '@angular/common';
 import { CharacterComponent } from './components/character/character.component';
 import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent} from "./components/footer/footer.component";
+import { CardComponent } from "./components/card/card.component";
 import { BackgroundComponent } from './components/background/background.component';
 import { DialogueComponent } from './components/dialogue/dialogue.component';
 import { SceneComponent } from './components/scene/scene.component';
 
 @NgModule({
-  declarations: [CharacterComponent, HeaderComponent, FooterComponent, BackgroundComponent, DialogueComponent, SceneComponent],
+  declarations: [
+    CharacterComponent, 
+    HeaderComponent,
+    FooterComponent,
+    CardComponent,
+    BackgroundComponent,
+    DialogueComponent,
+    SceneComponent
+  ],
   imports: [CommonModule],
-  exports: [SceneComponent, HeaderComponent, FooterComponent]
+  exports: [
+    SceneComponent,
+    HeaderComponent,
+    FooterComponent,
+    CardComponent
+  ]
 })
 export class SharedModule { }
