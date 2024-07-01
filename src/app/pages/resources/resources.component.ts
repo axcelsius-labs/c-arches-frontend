4bc7144785fb56145f4acadf542007fd08b3c763
ludachrismonte <ludachrismonte@gmail.com>
2024-07-01 17:16:44 -0400

    added data to resource cards, and extracted data to a const file

--- src/app/pages/resources/resources.component.ts
+++ src/app/pages/resources/resources.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { resources } from './resources.constants'
 
 @Component({
   selector: 'app-resources',
@@ -7,62 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./resources.component.scss']
 })
 export class ResourcesComponent implements OnInit {
-  
-  leftColumn = [
-    {
-      name: 'The National Domestic Violence Hotline',
-      subtitle: '24/7 confidential, and free',
-      description: ''
-    },
-    {
-      name: 'Crisis Text Line',
-      subtitle: 'Text trained counselors about anything that’s on your mind.',
-      description: ''
-    },
-    {
-      name: 'myPlanApp',
-      subtitle: 'If you or someone you are about is experiencing abuse in their relationship, download the myPlan app to help with safety decisions',
-      description: ''
-    },
-    {
-      name: 'The National Sexual Assault Hotline',
-      subtitle: '24/7 confidential and free support for survivors and loved ones.',
-      description: ''
-    },
-    {
-      name: 'LoveisRespect.org',
-      subtitle: '24/7 teen dating violence support.',
-      description: ''
-    }
-  ];
 
-  rightColumn = [
-    {
-      name: 'StrongHearts Native Helpline',
-      subtitle: 'For confidential domestic/sexual violence support, specifically for Native communities',
-      description: ''
-    },
-    {
-      name: 'Trans LifeLine',
-      subtitle: 'A peer-support hotline staffed exclusively by trans operators. The only crisis line with a policy against non-consensual active rescue.',
-      description: ''
-    },
-    {
-      name: 'Deaf Hotline',
-      subtitle: '24/7 chat for Deaf, DeafBlind, DeafDisabled',
-      description: ''
-    },
-    {
-      name: 'National Parent Helpline',
-      subtitle: 'Emotional support and advocacy for parents.',
-      description: ''
-    },
-    {
-      name: 'Futures Without Violence',
-      subtitle: '',
-      description: ''
-    }
-  ];
+  resources = resources;
   
   constructor() { }
 
