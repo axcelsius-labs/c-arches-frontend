c674f4a25f30dbe1e9fe5f83c62bc78d2d2c7c56
andrewsigurdson <asigu@umich.edu>
2024-07-09 10:40:35 -0500

    removing the angular folder and created a main container

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from "./components/header/header.component";
-import { FooterComponent} from "./components/footer/footer.component";
-import { CardComponent } from "./components/card/card.component";
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CardComponent } from './components/card/card.component';
 import { BackgroundComponent } from './components/background/background.component';
 import { DialogueComponent } from './components/dialogue/dialogue.component';
 import { SceneComponent } from './components/scene/scene.component';
-import { MonologueComponent } from "./components/monologue/monologue.component";
+import { MonologueComponent } from './components/monologue/monologue.component';
 import { MonologueGridComponent } from './components/monologue.grid/monologue.grid.component';
 import { Chapter30Component } from './components/chapter.3.0/chapter.3.0.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MainContainerComponent } from './components/main-container/main-container.component';
 
 @NgModule({
   declarations: [
@@ -23,18 +24,19 @@ import { MatDialogModule } from '@angular/material/dialog';
     SceneComponent,
     MonologueComponent,
     MonologueGridComponent,
-    Chapter30Component
+    Chapter30Component,
+    MainContainerComponent,
   ],
   imports: [CommonModule, MatIconModule, MatProgressBarModule, MatDialogModule],
   exports: [
     MonologueComponent,
     SceneComponent,
-    BackgroundComponent,
     HeaderComponent,
     FooterComponent,
     CardComponent,
     MonologueGridComponent,
     Chapter30Component,
-  ]
+    MainContainerComponent,
+  ],
 })
-export class SharedModule { }
+export class SharedModule {}
