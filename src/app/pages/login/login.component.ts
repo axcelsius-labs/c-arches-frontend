760940f52ff99bfefed6d34875631a23dd64ca44
ludachrismonte <ludachrismonte@gmail.com>
2024-07-10 19:57:21 -0400

    some route stuff for home and ending

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -17,7 +17,7 @@ export class LoginComponent {
       if (typeof window !== 'undefined') {
         localStorage.setItem('password', 'true');
       }
-      this.router.navigate(['/chapter', '0']); //update this line
+      this.router.navigate(['/welcome']);
     } else this.error = true;
   }
 }
