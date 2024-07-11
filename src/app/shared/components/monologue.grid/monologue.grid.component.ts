83fe9fe5d663e7d3fda6324a4d289d64ed73b389
andrewsigurdson <asigu@umich.edu>
2024-07-10 23:49:16 -0500

    removed comments

--- src/app/shared/components/monologue.grid/monologue.grid.component.ts
+++ src/app/shared/components/monologue.grid/monologue.grid.component.ts
@@ -23,26 +23,5 @@ export class MonologueGridComponent implements OnInit {
 
   select(index: number) {
     this.clickedOption.emit(this.section.gridOptionIndexes![index]);
-    // //go to the right
-    // this.dialogue.updateDialogLines(
-    //   this.section.data[this.currentBoxIndex].lines,
-    //   0,
-    // );
   }
-
-  // handleClickOrSpace(event?: Event): void {
-  //   if (this.dialogue.isAnimating$.value) {
-  //     this.dialogue.isAnimating$.next(false);
-  //   } else if (!this.dialogue.isAtSectionEnd()) {
-  //     this.dialogue.playNextDialogueLine();
-  //   }
-  // }
-
-  // @HostListener('document:keypress', ['$event'])
-  // handleKeyboardEvent(event: KeyboardEvent) {
-  //   if (event.code === 'Space') {
-  //     this.handleClickOrSpace();
-  //     event.stopPropagation();
-  //   }
-  // }
 }
