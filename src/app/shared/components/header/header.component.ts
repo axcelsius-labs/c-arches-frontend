734b1e2aa87323e13392e3737b9f4f4b8728c9d9
andrewsigurdson <asigu@umich.edu>
2024-07-15 21:49:29 -0500

    fixed the header spacing

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,25 +1,64 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import {DialogueService} from "../../services/dialogue.service";
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { DialogueService } from '../../services/dialogue.service';
+import { Subscription, filter } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent implements OnInit {
-  
-  @Input() title: string = "C-ARCHES";
-  
+  @Input() title: string = 'C-ARCHES';
+
   highlightExit: boolean = false;
-  
-  constructor(private router: Router,
-              private dialogueService: DialogueService) { }
+  private currentLineSubscription!: Subscription;
+  private routerSubscription!: Subscription;
+  private isIntroRoute: boolean = false;
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private dialogueService: DialogueService,
+  ) {}
 
   ngOnInit() {
-    this.dialogueService.currentLine$.subscribe((value) => {
-      this.highlightExit = value.params.includes("highlightExit");
-    });
+    this.currentLineSubscription = this.dialogueService.currentLine$.subscribe(
+      (line) => {
+        if (this.isIntroRoute) {
+          this.highlightExit =
+            line.params && line.params.includes('highlightExit');
+        } else {
+          this.highlightExit = false;
+        }
+      },
+    );
+
+    // Subscribe to router events to detect navigation changes
+    this.routerSubscription = this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.checkRoute();
+      });
+
+    // Initial check for the route
+    this.checkRoute();
+  }
+
+  ngOnDestroy(): void {
+    // Unsubscribe from all subscriptions to prevent memory leaks
+    this.currentLineSubscription.unsubscribe();
+    this.routerSubscription.unsubscribe();
+  }
+
+  private checkRoute(): void {
+    const urlSegments = this.route.snapshot.url;
+    this.isIntroRoute =
+      urlSegments.length > 0 && urlSegments[0].path === 'intro';
+
+    // Reset showHighlightExit if not in Intro route
+    if (!this.isIntroRoute) {
+      this.highlightExit = false;
+    }
   }
 
   goToHomeScreen() {
@@ -29,7 +68,7 @@ export class HeaderComponent implements OnInit {
   goToResources() {
     this.router.navigate(['/resources']);
   }
-  
+
   exit() {
     localStorage.clear();
     window.location.href = 'https://weather.com/';
